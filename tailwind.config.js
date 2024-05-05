81e018899cafb8acd8dbd30ebc279d4083461f91
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2024-05-06 01:42:45 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const {nextui, colors} = require("@nextui-org/react");
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#0097ce'
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+
+};
